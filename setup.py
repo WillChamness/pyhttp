6f55dad32ee0db5649aaebf42bf37adbf479eea0
WillChamness <wzchamness@gmail.com>
2025-06-18 23:14:00 -0400

    build: add setup.py and dev dependency

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pyhttp",
+    version="0.1.0",
+    packages=["pyhttp"],
+    install_requires=[],
+    entry_points={
+        "console_scripts": ["pyhttp = pyhttp.cli:parse_args"],
+    },
+)
